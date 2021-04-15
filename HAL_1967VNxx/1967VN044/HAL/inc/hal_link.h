532ddd241a1e8252c04269363f62e5bd4c79d0b1
startmilandr2 <smirnov.art8@gmail.com>
2021-04-15 13:29:35 +0300

    update 2.02.00

--- HAL_1967VNxx/1967VN044/HAL/inc/hal_link.h
+++ HAL_1967VNxx/1967VN044/HAL/inc/hal_link.h
@@ -17,6 +17,8 @@
 
 #include <stdint.h>
 
+#define Link_BitOrder_OddFrontEvenCut Link_BitOrder_EvenFrontOddCut  // for backward compatibility
+
 typedef enum {Link_CheckSum_Dis = 0,	Link_CheckSum_En = LRX_VEN} 	Link_CheckSum_type;
 typedef enum {Link_CheckBCMP_Dis = 0,	Link_CheckBCMP_En = LRX_BCMP} 	Link_CheckBCMP_type;
 typedef enum {Link_TOIT_Dis = 0,		Link_TOIT_En = LRX_TOEN} 		Link_TOIT_type;
@@ -28,7 +30,7 @@ typedef enum {Link_GpsClk_Dis = 0,     	Link_GpsClk_En = LRX_GPSCLKEN } Link_Gps
 typedef enum {Link_RxDest_Buff = 0,		Link_RxDest_UpDown = ( 1 << 11 ) } Link_RxDest_type;
 typedef enum {Link_AdcDataSize_10b = ( 3 << 12 ),  Link_AdcDataSize_12b = ( 2 << 12 ), 	Link_AdcDataSize_14b = ( 1 << 12 ), Link_AdcDataSize_16b = 0 } Link_AdcDataSize_type;
 typedef enum {Link_Rcode_Dis = 0,		Link_Rcode_En = ( 1 << 14 ) } Link_Rcode_type;
-typedef enum {Link_BitOrder_LoFrontHiCut = 0,		Link_BitOrder_OddFrontEvenCut = ( 1 << 15 )	} Link_BitOrder_type;
+typedef enum {Link_BitOrder_LoFrontHiCut = 0,		Link_BitOrder_EvenFrontOddCut = ( 1 << 15 )	} Link_BitOrder_type;
 typedef enum {Link_TxDest_Buff = 0, 	Link_TxDest_UpDown = ( 1 << 11 ) } Link_TxDest_type;
 typedef enum {Link_Tcode_Dis = 0,		Link_Tcode_En = ( 1 << 12 ) } Link_Tcode_type;
 typedef enum {Link_CheckACKI_Dis = ( 1 << 13 ),	Link_CheckACKI_En = 0 } Link_CheckACKI_type;
@@ -47,7 +49,7 @@ typedef volatile struct
 	Link_GpsClk_type 			GpsClk;																// Разрешение генерации клока GPS
 	Link_RxDest_type 			RxDest;																// Выбор приемника данных
 	Link_AdcDataSize_type 		AdcDataSize;														// Режим для ADC 8 бит шины данных
-	Link_Rcode_type 			Rcode;																// Преобразование прямого кода в дополнительный
+	Link_Rcode_type 			Rcode;																// Преобразование смещенного кода в дополнительный для 8 бит шины данных
 	Link_BitOrder_type 			BitOrder;															// Порядок выдачи бит
 } LinkRxEx_type;
 
@@ -63,7 +65,7 @@ typedef volatile struct
 typedef volatile struct
 {
 	Link_TxDest_type 			TxSource;															// Выбор источника данных для передачи
-	Link_Tcode_type 			Tcode;																// Преобразование прямого кода в дополнительный
+	Link_Tcode_type 			Tcode;																// Преобразование дополнительного кода в смещенный
 	Link_CheckACKI_type 		CheckACKI;															// Анализ сигнала LxACKI
 } LinkTxEx_type;
 
